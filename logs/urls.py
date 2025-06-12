f97fafbea606efad195feba3056bc49ecfc8b861
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-06-12 12:23:21 +0200

    added templates and changed path to home

--- logs/urls.py
+++ logs/urls.py
@@ -1,6 +1,7 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.log_list, name='log_list'),
+    path('log/', views.log_fish_session, name='log_fish'),
 ]
