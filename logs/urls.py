b7aac498497dd82a4853f43824a4ee238667d3a2
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-06-13 15:09:09 +0200

    created session_list.html and session_stats.html and styled with css

--- logs/urls.py
+++ logs/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home, dashboard, log_fish_session
+from .views import home, dashboard, log_fish_session, session_list, session_stats
 
 urlpatterns = [
     path('', home, name='home'),
     path('log/', log_fish_session, name='log_fish'),
     path('dashboard/', dashboard, name='dashboard'),
+    path('sessions/', session_list, name='session_list'),
+    path('stats/', session_stats, name='session_stats'),
     
 ]
