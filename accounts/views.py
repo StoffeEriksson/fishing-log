59f408cccdd39f4d55c6572b46096a9ada9bb721
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-06-14 23:37:08 +0200

    created delete account and fixed view.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 # Create your views here.
+@login_required
+def delete_account(request):
+    if request.method == 'POST':
+        request.user.delete()
+        messages.success(request, "Your account has been deleted.")
+        return redirect('home')
+    return render(request, 'accounts/delete_account.html')
